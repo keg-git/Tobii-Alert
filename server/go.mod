4bcf8f6cf01330988dcb8955dd83cdaabec5ebd7
keg-git <keg.git@proton.me>
2025-01-31 18:09:17 -0500

    added the folders and a main.go

---
+++ server/go.mod
@@ -0,0 +1,5 @@
+module main
+
+go 1.23.5
+
+require github.com/tosone/minimp3 v1.0.2 // indirect
---
+++ server/main.go
@@ -0,0 +1,11 @@
+package main
+
+import(
+    "fmt"
+    // "net/http"
+    // "github.com/tosone/minimp3"
+)
+
+func main() {
+    fmt.Print("Hello World!\n")
+}
