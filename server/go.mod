a8fcfe39e3243b313898a993f30643c6bc2aa031
keg-git <keg.git@proton.me>
2025-02-27 16:37:03 -0500

    got the preliminary functionality ok

--- server/go.mod
+++ server/go.mod
@@ -2,4 +2,12 @@ module main
 
 go 1.23.5
 
-require github.com/tosone/minimp3 v1.0.2 // indirect
+require github.com/gopxl/beep v1.4.1
+
+require (
+	github.com/ebitengine/oto/v3 v3.1.0 // indirect
+	github.com/ebitengine/purego v0.7.1 // indirect
+	github.com/hajimehoshi/go-mp3 v0.3.4 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+)
--- server/main.go
+++ server/main.go
@@ -2,10 +2,68 @@ package main
 
 import(
     "fmt"
-    // "net/http"
-    // "github.com/tosone/minimp3"
+    "net/http"
+    "github.com/gopxl/beep"
+    "github.com/gopxl/beep/mp3"
+    "github.com/gopxl/beep/speaker"
+    "time"
+    "log"
+    "os"
 )
 
+func alert(reason string) {
+    tunes, err := os.Open("/home/keg/Music/notification/alarm.mp3")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    streamer, format, err := mp3.Decode(tunes)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer streamer.Close()
+
+    loop := beep.Loop(-1, streamer)
+    speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+
+    speaker.Play(loop)
+
+    fmt.Print("Dad Currently needs you for ", reason, "!\nGet off your ass now")
+    fmt.Scanln()
+    fmt.Print("Stopped go see Dad\n")
+
+    speaker.Clear()
+    return
+}
+
+func simplealert(w http.ResponseWriter, r *http.Request) {
+    alert("something")
+}
+
+func suction(w http.ResponseWriter, r *http.Request) {
+    alert("suction")
+}
+
+func readjust(w http.ResponseWriter, r *http.Request) {
+    alert("readjusting")
+}
+
+func bathroom(w http.ResponseWriter, r *http.Request) {
+    alert("taking a shit")
+}
+
+func shutoff(w http.ResponseWriter, r *http.Request) {
+    speaker.Clear()
+}
 func main() {
-    fmt.Print("Hello World!\n")
+
+    http.HandleFunc("/", simplealert)
+    http.HandleFunc("/suction", suction)
+    http.HandleFunc("/bathroom", bathroom)
+    http.HandleFunc("/readjust", readjust)
+    http.HandleFunc("/shutoff", shutoff)
+
+    fmt.Print("Starting Server\n")
+    log.Fatal(http.ListenAndServe(":8080", nil))
+    // alert()
 }
